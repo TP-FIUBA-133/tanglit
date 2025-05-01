87e01c4e0025de8de1d9c2978305206cb195a41f
avilagaston9 <gaston.avila@lambdaclass.com>
2025-05-01 15:05:28 -0300

    Basic parse using regex

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,25 @@
+use backend::parser::parse_blocks_from_file;
+use std::fs::write;
+
+const INPUT_FILE: &str = "input_file.txt";
+const OUTPUT_FILE: &str = "output_file.txt";
+
 fn main() {
-    println!("Hello, world!");
+    let blocks = match parse_blocks_from_file(INPUT_FILE) {
+        Ok(blocks) => blocks,
+        Err(e) => {
+            println!("Error parsing blocks: {}", e);
+            return;
+        }
+    };
+
+    let output = blocks
+        .iter()
+        .map(|block| format!("{}\n", block))
+        .collect::<String>();
+
+    match write(OUTPUT_FILE, output) {
+        Ok(_) => println!("Blocks written to {}", OUTPUT_FILE),
+        Err(e) => println!("Error writing to file: {}", e),
+    };
 }
