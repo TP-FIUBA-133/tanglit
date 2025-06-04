aa477e95a98d2abddd82af3ffee88ee28d2dee9f
avilagaston9 <gaston.avila@lambdaclass.com>
2025-06-04 00:33:31 -0300

    feat: tangle block with imports

--- backend/src/tangle.rs
+++ backend/src/tangle.rs
@@ -1,4 +1,4 @@
-use crate::parser::code_block::CodeBlock;
+use crate::{errors::TangleError, parser::code_block::CodeBlock};
 
 pub fn tangle_blocks(blocks: Vec<CodeBlock>) -> String {
     let mut tangle = String::new();
@@ -7,6 +7,43 @@ pub fn tangle_blocks(blocks: Vec<CodeBlock>) -> String {
         tangle.push('\n');
     }
     tangle
+}
+
+pub fn tangle_block(block: String, blocks: &[CodeBlock]) -> Result<String, TangleError> {
+    // Search block
+    let code_block = blocks
+        .iter()
+        .find(|b| b.tag.clone().unwrap_or_default() == block)
+        .ok_or_else(|| TangleError::BlockNotFound(block))?;
+
+    // Search imported blocks
+    let imported_blocks: Vec<CodeBlock> = blocks
+        .iter()
+        .filter(|b| {
+            code_block
+                .imports
+                .contains(&b.tag.clone().unwrap_or_default())
+        })
+        .cloned()
+        .collect();
+
+    // Tangle imports
+    let mut tangle = String::new();
+    for block in imported_blocks {
+        tangle.push_str(&block.code);
+        tangle.push('\n');
+    }
+
+    tangle.push('\n');
+
+    // Add the main block code
+    tangle.push_str("int main() {\n"); // TODO: this should be configurable
+    tangle.push_str(&code_block.code); // TODO: handle indentation
+    tangle.push('\n');
+    tangle.push_str("\treturn 0;");
+    tangle.push_str("\n}\n");
+
+    Ok(tangle)
 }
 
 #[cfg(test)]
