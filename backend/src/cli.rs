73177d5c575d122f5670ef2df44abd9600aaeff3
avilagaston9 <gaston.avila@lambdaclass.com>
2025-06-14 19:08:38 -0300

    feat: add basic CLI

--- backend/src/cli.rs
+++ backend/src/cli.rs
@@ -1,21 +1,37 @@
-use clap::{Args, Parser, Subcommand};
+use clap::{Args, Parser};
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
 #[command(propagate_version = true)]
-struct Cli {
-    #[command(subcommand)]
-    command: Commands,
-}
-
-#[derive(Subcommand)]
-enum Commands {
-    /// Tangle code blocks from a file
-    Tangle(TangleArgs),
+pub struct Cli {
+    #[command(flatten)]
+    pub tangle_args: TangleArgs,
 }
 
 #[derive(Args)]
-struct TangleArgs {
-    file_path: String,
-    main_block: String,
+pub struct TangleArgs {
+    #[arg(
+        long,
+        value_name = "INPUT_FILE_PATH",
+        help = "Path to the input markdown file.",
+        help_heading = "Tangle Args",
+        env = "INPUT_FILE_PATH"
+    )]
+    pub input_file_path: String,
+    #[arg(
+        long,
+        value_name = "OUTPUT_DIR",
+        help = "Path to the directory where the output file will be saved.",
+        help_heading = "Tangle Args",
+        env = "OUTPUT_DIR"
+    )]
+    pub output_dir: String,
+    #[arg(
+        long,
+        value_name = "TARGET_BLOCK",
+        help = "Tag of the code block to tangle.",
+        help_heading = "Tangle Args",
+        env = "TARGET_BLOCK"
+    )]
+    pub target_block: String,
 }
