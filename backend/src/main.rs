02dd2a07fe153e79e02be6136ce6028b4f984627
Avila Gastón <72628438+avilagaston9@users.noreply.github.com>
2025-06-20 15:34:14 -0300

    feat: tangle block with imports (#40)

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,8 +1,8 @@
-use backend::{parser::parse_blocks_from_file, tangle::tangle_blocks};
+use backend::{parser::parse_blocks_from_file, tangle::tangle_block};
 use std::fs::write;
 
-const INPUT_FILE: &str = "./test_data/test_file.md";
-const OUTPUT_FILE: &str = "./test_data/output_file.rs";
+const INPUT_FILE: &str = "./test_data/test_file_2.md";
+const OUTPUT_FILE: &str = "./test_data/output_file_2.c";
 
 fn main() {
     // Parse blocks from the input file
@@ -15,7 +15,11 @@ fn main() {
     };
 
     // Tangle blocks
-    let output = tangle_blocks(blocks);
+    let Ok(output) = tangle_block(String::from("print"), &blocks)
+        .inspect_err(|e| println!("Error tangling blocks: {e}"))
+    else {
+        return;
+    };
 
     // Write the output to a file
     match write(OUTPUT_FILE, output) {
