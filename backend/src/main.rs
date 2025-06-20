e06f86505dcf53e1369cd75dfe6aaa443c1fb33f
Avila Gastón <72628438+avilagaston9@users.noreply.github.com>
2025-06-20 16:15:40 -0300

    feat: add basic CLI (#42)

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,12 +1,15 @@
-use backend::{parser::parse_blocks_from_file, tangle::tangle_block};
-use std::fs::write;
-
-const INPUT_FILE: &str = "./test_data/test_file_2.md";
-const OUTPUT_FILE: &str = "./test_data/output_file_2.c";
+use backend::{cli::Cli, parser::parse_blocks_from_file, tangle::tangle_block};
+use clap::Parser;
+use std::{
+    fs::write,
+    path::{Path, PathBuf},
+};
 
 fn main() {
+    let Cli { tangle_args } = Cli::parse();
+
     // Parse blocks from the input file
-    let blocks = match parse_blocks_from_file(INPUT_FILE) {
+    let blocks = match parse_blocks_from_file(&tangle_args.input_file_path) {
         Ok(blocks) => blocks,
         Err(e) => {
             println!("Error parsing blocks: {}", e);
@@ -15,15 +18,22 @@ fn main() {
     };
 
     // Tangle blocks
-    let Ok(output) = tangle_block(String::from("print"), &blocks)
+    let Ok(output) = tangle_block(&tangle_args.target_block, &blocks)
         .inspect_err(|e| println!("Error tangling blocks: {e}"))
     else {
         return;
     };
 
     // Write the output to a file
-    match write(OUTPUT_FILE, output) {
-        Ok(_) => println!("Blocks written to {}", OUTPUT_FILE),
+    let output_file_path = get_output_file_path(&tangle_args.output_dir, &tangle_args.target_block);
+    match write(&output_file_path, output) {
+        Ok(_) => println!("Blocks written to {}", output_file_path.display()),
         Err(e) => println!("Error writing to file: {}", e),
     };
+}
+
+// TODO: We should get the output file path based on the language
+fn get_output_file_path(output_file_path: &str, main_block: &str) -> PathBuf {
+    let output_file_path = Path::new(output_file_path);
+    output_file_path.join(format!("{main_block}.c"))
 }
