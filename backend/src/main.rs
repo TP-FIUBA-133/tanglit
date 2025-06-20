ab4c5b37bd1e27f9541fb3c3bdc1df8f559c8712
Christian Pedersen <chrishaig14@gmail.com>
2025-06-20 16:47:18 -0300

    rm stuff from main.rs

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,15 +1,10 @@
-use backend::parser::input_to_mdast;
-use backend::{parser::parse_blocks_from_file, parser::slides::get_slides, tangle::tangle_block};
+use backend::{parser::parse_blocks_from_file, tangle::tangle_block};
 use std::fs::write;
 
 const INPUT_FILE: &str = "./test_data/test_file_2.md";
 const OUTPUT_FILE: &str = "./test_data/output_file_2.c";
 
 fn main() {
-    let input = std::fs::read_to_string(INPUT_FILE).expect("Failed to read file");
-    let ast = input_to_mdast(&input).expect("Failed to parse input");
-    let _slides = get_slides(&ast, input.as_str());
-
     // Parse blocks from the input file
     let blocks = match parse_blocks_from_file(INPUT_FILE) {
         Ok(blocks) => blocks,
