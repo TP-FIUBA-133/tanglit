9bcb56d2fbff5de95f48dbc9b35489ab075a3fa2
Christian Pedersen <chrishaig14@gmail.com>
2025-06-20 15:52:17 -0300

    start_line var

--- backend/src/parser/slides/mod.rs
+++ backend/src/parser/slides/mod.rs
@@ -22,18 +22,19 @@ pub fn get_slides(mdast: &Node, input: &str) -> Vec<Slide> {
         let mut new_slide = None;
         if let Node::Heading(heading) = child {
             if heading.depth == 1 {
+                let start_line = heading.position.as_ref().unwrap().start.line;
                 if heading.children.is_empty() {
                     // it's empty, we still have a slide, but with no title
                     new_slide = Some(Slide {
                         title: None,
                         content: vec![],
-                        start_line: heading.position.as_ref().unwrap().start.line,
+                        start_line,
                     });
                 } else if let Node::Text(_) = &heading.children[0] {
                     new_slide = Some(Slide {
                         title: Some(i),
                         content: vec![],
-                        start_line: heading.position.as_ref().unwrap().start.line,
+                        start_line,
                     });
                 }
             }
