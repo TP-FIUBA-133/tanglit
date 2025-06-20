59c58db84af235a53f5eca01aa634ea6ad817249
Christian Pedersen <chrishaig14@gmail.com>
2025-06-20 15:36:29 -0300

    use get_slides to make linter happy

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,10 +1,15 @@
-use backend::{parser::parse_blocks_from_file, tangle::tangle_blocks};
+use backend::parser::input_to_mdast;
+use backend::{parser::parse_blocks_from_file, parser::slides::get_slides, tangle::tangle_blocks};
 use std::fs::write;
 
 const INPUT_FILE: &str = "./test_data/test_file.md";
 const OUTPUT_FILE: &str = "./test_data/output_file.rs";
 
 fn main() {
+    let input = std::fs::read_to_string(INPUT_FILE).expect("Failed to read file");
+    let ast = input_to_mdast(&input).expect("Failed to parse input");
+    let _slides = get_slides(&ast, input.as_str());
+
     // Parse blocks from the input file
     let blocks = match parse_blocks_from_file(INPUT_FILE) {
         Ok(blocks) => blocks,
