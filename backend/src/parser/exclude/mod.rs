0ce1e23ce63d8a81327f803b429433fb965e4423
Christian Pedersen <chrishaig14@gmail.com>
2025-06-21 15:07:28 -0300

    make code clearer

--- backend/src/parser/exclude/mod.rs
+++ backend/src/parser/exclude/mod.rs
@@ -1,3 +1,4 @@
+use crate::parser::parse_input;
 use log::debug;
 use markdown::mdast::{List, Node, Paragraph, Text};
 
@@ -14,6 +15,18 @@ fn get_ast(input: &str) -> Node {
     markdown::to_mdast(input, &markdown::ParseOptions::mdx()).unwrap()
 }
 
+fn get_paragraph_first_line_marker(paragraph: &Paragraph) -> Option<String> {
+    if let Some(Node::Text(text)) = paragraph.children.first() {
+        let Some(first_line) = text.value.lines().next() else {
+            return None;
+        };
+        let marker_regex =
+            regex::Regex::new(r"^.* (%[ipl]?)").expect("Failed to compile marker regex");
+        return Some(marker_regex.captures(&first_line)?[1].to_string());
+    }
+    None
+}
+
 fn process_list(list_node: &List) -> Option<List> {
     let mut new_list = List {
         children: vec![],
@@ -22,30 +35,54 @@ fn process_list(list_node: &List) -> Option<List> {
         start: None,
         spread: false,
     };
-    'list_items_loop: for item in &list_node.children {
-        if let Node::ListItem(list_item) = item {
-            let mut new_item = list_item.clone();
-            for child in list_item.children.iter() {
-                if let Node::Paragraph(p) = child {
-                    let first_child = p.children.first();
-                    if let Some(Node::Text(t)) = first_child {
-                        let first_line = t.value.lines().next();
-                        if let Some(line) = first_line {
-                            if line.ends_with(EXCLUDE_LIST_MARKER) {
-                                return None;
-                            }
-                            if line.ends_with(EXCLUDE_LIST_ITEM_MARKER) {
-                                continue 'list_items_loop; // Exclude this list item completely, no need to process the rest of its children
-                            }
-                        }
-                    }
-                }
+    if list_node.children.is_empty() {
+        return Some(new_list);
+    }
+    // check the first line of the first item in the list for the EXCLUDE_LIST_MARKER
+    let Node::ListItem(first_item) = &list_node.children[0] else {
+        panic!("Expected a ListItem")
+    };
+    if !first_item.children.is_empty() {
+        if let Node::Paragraph(p) = &first_item.children[0] {
+            let first_line_marker = get_paragraph_first_line_marker(&p);
+            if first_line_marker == Some(EXCLUDE_LIST_MARKER.into()) {
+                // Exclude the entire list if the first line of the first item has the marker
+                return None;
             }
-            new_item.children = process_children(&list_item.children);
-            new_list.children.push(Node::ListItem(new_item));
         }
     }
+
+    'list_items_loop: for item in &list_node.children {
+        let Node::ListItem(list_item) = item else {
+            panic!("Expected a ListItem");
+        };
+        if let Node::Paragraph(p) = &list_item.children[0] {
+            let item_marker = get_paragraph_first_line_marker(p);
+            if item_marker == Some(EXCLUDE_LIST_ITEM_MARKER.into()) {
+                // Exclude this list item completely, no need to process the rest of its children
+                continue 'list_items_loop;
+            }
+        }
+        let mut new_item = list_item.clone();
+        new_item.children = process_children(&list_item.children);
+        new_list.children.push(Node::ListItem(new_item));
+    }
     Some(new_list)
+}
+
+fn exclude_lines_from_text(text: &Text) -> Text {
+    let mut content = String::new();
+    for line in text.value.lines() {
+        if line.ends_with(EXCLUDE_LINE_MARKER) {
+            continue;
+        } else {
+            content.push_str((line.to_string() + "\n").as_str());
+        }
+    }
+    Text {
+        value: content.trim().to_string(),
+        position: None,
+    }
 }
 
 fn process_paragraph(paragraph: &Paragraph) -> Option<Paragraph> {
@@ -53,33 +90,14 @@ fn process_paragraph(paragraph: &Paragraph) -> Option<Paragraph> {
         children: vec![],
         position: None,
     };
+    if get_paragraph_first_line_marker(paragraph) == Some(EXCLUDE_PARAGRAPH_MARKER.into()) {
+        return None; // Exclude the entire paragraph if the first line has the marker
+    }
     for child in &paragraph.children {
-        if let Node::Text(text) = child {
-            let mut lines = text.value.lines();
-            let first_line = lines.next();
-            if let Some(line) = first_line {
-                // debug!("First line {}", line);
-                if line.ends_with(EXCLUDE_PARAGRAPH_MARKER) {
-                    // debug!("Excluding this paragraph: {}", line);
-                    return None;
-                }
-            }
-            let mut content = String::new();
-            for line in text.value.lines() {
-                if line.ends_with(EXCLUDE_LINE_MARKER) {
-                    // debug!("Excluding this line: {}", line);
-                    continue;
-                } else {
-                    content.push_str((line.to_string() + "\n").as_str());
-                }
-            }
-            let new_text = Text {
-                value: content.trim().to_string(),
-                position: text.position.clone(),
-            };
-            if !new_text.value.is_empty() {
-                new_paragraph.children.push(Node::Text(new_text));
-            }
+        let Node::Text(text) = child else { continue };
+        let new_text = exclude_lines_from_text(text);
+        if !new_text.value.is_empty() {
+            new_paragraph.children.push(Node::Text(new_text));
         }
     }
 
