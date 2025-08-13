1dc5e89255d77c2763400efd6c508383f33679d9
TomBPotochek <TomBPotochek@gmail.com>
2025-08-13 15:15:51 -0300

    refactor: rename configuration functions

--- backend/src/configuration.rs
+++ backend/src/configuration.rs
@@ -3,7 +3,7 @@ mod user;
 use std::io;
 use user::create_configuration_dirs;
 
-pub use user::{get_default_config_dir, get_default_temp_dir};
+pub use user::{get_config_dir, get_temp_dir};
 
 /// Creates and initializes the default configuration directories.
 pub fn init_configuration() -> io::Result<()> {
--- backend/src/execution.rs
+++ backend/src/execution.rs
@@ -1,4 +1,4 @@
-use crate::configuration::get_default_temp_dir;
+use crate::configuration::get_temp_dir;
 use crate::doc::CodeBlocksDoc;
 use crate::doc::DocError;
 use crate::doc::TangleError;
@@ -14,7 +14,7 @@ const DEFAULT_INDENT_CHARACTER: char = ' ';
 
 /// Writes the contents to a file to a `tmp` directory in the current directory.
 fn write_file(contents: String, name: &str, lang: &Language) -> io::Result<std::path::PathBuf> {
-    let tmp_dir = get_default_temp_dir();
+    let tmp_dir = get_temp_dir();
 
     // Create the file path using the target block name
     let ext = match lang {
