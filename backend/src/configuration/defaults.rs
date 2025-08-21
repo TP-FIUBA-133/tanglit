656e75ba89051b719dcaa7b1abc8298c3bb212f2
Christian Pedersen <chrishaig14@gmail.com>
2025-08-21 13:04:02 -0300

    extract_soft in default.rs (we can remove the patched include_dir)

--- backend/src/configuration/defaults.rs
+++ backend/src/configuration/defaults.rs
@@ -1,12 +1,29 @@
-use std::{io, path::Path};
-
-use include_dir::{Dir, include_dir};
+use include_dir::{include_dir, Dir, DirEntry};
+use std::{fs, io, path::Path};
 
 static CONFIG_RESOURCES_DIR: Dir<'_> = include_dir!("$CARGO_MANIFEST_DIR/resources/config");
 
-pub fn create_default_config(config_dir_path: &Path) -> io::Result<()> {
-    let config_subdir = &CONFIG_RESOURCES_DIR;
+pub fn extract_soft(source: &Dir, dest_path: &Path) -> io::Result<()> {
+    for entry in source.entries() {
+        let path = dest_path.join(entry.path());
 
-    config_subdir.extract_soft(config_dir_path)?;
+        match entry {
+            DirEntry::Dir(d) => {
+                fs::create_dir_all(&path)?;
+                extract_soft(d, dest_path)?;
+            }
+            DirEntry::File(file) => {
+                if !path.exists() {
+                    fs::write(path, file.contents())?;
+                }
+            }
+        }
+    }
+    Ok(())
+}
+
+pub fn create_default_config(config_dir_path: &Path) -> io::Result<()> {
+    fs::create_dir_all(config_dir_path)?;
+    extract_soft(&CONFIG_RESOURCES_DIR, config_dir_path)?;
     Ok(())
 }
