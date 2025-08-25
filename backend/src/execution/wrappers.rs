38f93336716bbf96d092d4c5f3dc64312a266dd6
Christian Pedersen <chrishaig14@gmail.com>
2025-08-25 12:27:52 -0300

    fix format

--- backend/src/execution/wrappers.rs
+++ backend/src/execution/wrappers.rs
@@ -138,7 +138,7 @@ mod tests {
         println!("Using config path: {}", config_path);
         with_var("TANGLIT_CONFIG_DIR", Some(config_path), || {
             let tangle = make_executable_code(&main, &CodeBlocks::from_codeblocks(blocks)).unwrap();
-            assert_eq ! (
+            assert_eq!(
                 tangle,
                 "#include <stdio.h>\n\n\nint main(){\n    int x;\n    x = 42;\n    printf(\"Hello, world!: %d\",x);\n    return 0;\n}\n"
                 .to_string()
