ccefd85096edc2f72ef24ef43ed9f62f81696f79
Christian Pedersen <chrishaig14@gmail.com>
2025-10-03 19:35:18 -0300

    fix

--- frontend/src/MarkdownEditor.vue
+++ frontend/src/MarkdownEditor.vue
@@ -111,7 +111,7 @@ function add_output_to_markdown(edit: Edit) {
   editor.value?.executeEdits("embed-result", [
     {
       range: new monaco.Range(edit.start_line, 1, edit.end_line, 1),
-      text: edit.content,
+      text: edit.content + "\n",
       forceMoveMarkers: true,
     },
   ]);
