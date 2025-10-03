229cefcb81ce368df0100dafa52a623637c783f1
Avila Gastón <72628438+avilagaston9@users.noreply.github.com>
2025-10-02 22:36:02 -0300

    feat(backend): exclude content from slides (#107)

--- backend/src/cli.rs
+++ backend/src/cli.rs
@@ -12,8 +12,6 @@ pub struct Cli {
 pub enum Commands {
     #[command(about = "Tangle a specific code block from a markdown file and export to a file")]
     Tangle(TangleArgs),
-    #[command(about = "Exclude parts with % markers from input markdown file")]
-    Exclude(ExcludeArgs),
     #[command(about = "Execute a specific code block from a markdown file and read its output")]
     Execute(ExecuteArgs),
     #[command(about = "Generates a PDF from an markdown file, skipping the items with % markers")]
@@ -56,20 +54,6 @@ pub struct TangleArgs {
         env = "TARGET_BLOCK"
     )]
     pub target_block: String,
-}
-
-#[derive(Args)]
-pub struct ExcludeArgs {
-    #[command(flatten)]
-    pub general: GeneralArgs,
-    #[arg(
-        long,
-        value_name = "OUTPUT_FILE_PATH",
-        help = "Path to the file where the output will be saved.",
-        help_heading = "Exclude Args",
-        env = "OUTPUT_FILE_PATH"
-    )]
-    pub output_file_path: String,
 }
 
 #[derive(Args)]
--- backend/src/doc/parser/exclude/test.rs
+++ backend/src/doc/parser/exclude/test.rs
@@ -6,12 +6,12 @@ mod tests {
     use crate::doc::parser::parse_from_string;
     use std::path::Path;
 
-    fn exclude_from_markdown(input: &str) -> Node {
+    fn exclude_from_markdown(input: &str, target: FilterTarget) -> Node {
         let mdast = parse_from_string(input).expect("Failed to parse markdown");
-        exclude_from_ast(&mdast)
+        exclude_from_ast(&mdast, target)
     }
 
-    fn test_exclusions_file(file: &str) {
+    fn test_exclusions_file(file: &str, target: FilterTarget) {
         let file_path = Path::new(file!()); // "tests/my_test.rs"
         let dir = file_path.parent().unwrap().join("test_files");
         let in_file = dir.join(format!("{file}-in.md"));
@@ -21,57 +21,66 @@ mod tests {
         let expected_output = std::fs::read_to_string(&out_file)
             .expect(format!("Failed to read {}", out_file.display()).as_str());
 
-        let ast_with_exclusions = exclude_from_markdown(input.as_str());
+        let ast_with_exclusions = exclude_from_markdown(input.as_str(), target);
         let actual_output = mdast_util_to_markdown::to_markdown(&ast_with_exclusions)
             .expect("Failed to convert to markdown");
-        let x = actual_output.trim();
+        let actual_output = actual_output.trim();
         assert_eq!(
-            expected_output, x,
+            expected_output, actual_output,
             "Output does not match expected for {file}"
         );
     }
 
     #[test_log::test]
     fn test_exclusions_1() {
-        test_exclusions_file("test_1")
+        test_exclusions_file("doc/test_1", FilterTarget::Doc);
+        test_exclusions_file("slides/test_1", FilterTarget::Slides);
     }
     #[test_log::test]
     fn test_exclusions_2() {
-        test_exclusions_file("test_2")
+        test_exclusions_file("doc/test_2", FilterTarget::Doc);
+        test_exclusions_file("slides/test_2", FilterTarget::Slides);
     }
 
     #[test_log::test]
     fn test_exclusions_3() {
-        test_exclusions_file("test_3")
+        test_exclusions_file("doc/test_3", FilterTarget::Doc);
+        test_exclusions_file("slides/test_3", FilterTarget::Slides);
     }
 
     #[test_log::test]
     fn test_exclusions_4() {
-        test_exclusions_file("test_4")
+        test_exclusions_file("doc/test_4", FilterTarget::Doc);
+        test_exclusions_file("slides/test_4", FilterTarget::Slides);
     }
 
     #[test_log::test]
     fn test_exclusions_5() {
-        test_exclusions_file("test_5")
+        test_exclusions_file("doc/test_5", FilterTarget::Doc);
+        test_exclusions_file("slides/test_5", FilterTarget::Slides);
     }
 
     #[test_log::test]
     fn test_exclusions_6() {
-        test_exclusions_file("test_6")
+        test_exclusions_file("doc/test_6", FilterTarget::Doc);
+        test_exclusions_file("slides/test_6", FilterTarget::Slides);
     }
 
     #[test_log::test]
     fn test_exclusions_7() {
-        test_exclusions_file("test_7")
+        test_exclusions_file("doc/test_7", FilterTarget::Doc);
+        test_exclusions_file("slides/test_7", FilterTarget::Slides);
     }
 
     #[test_log::test]
     fn test_exclusions_8() {
-        test_exclusions_file("test_8")
+        test_exclusions_file("doc/test_8", FilterTarget::Doc);
+        test_exclusions_file("slides/test_8", FilterTarget::Slides);
     }
 
     #[test_log::test]
     fn test_exclusions_9() {
-        test_exclusions_file("test_9")
+        test_exclusions_file("doc/test_9", FilterTarget::Doc);
+        test_exclusions_file("slides/test_9", FilterTarget::Slides);
     }
 }
