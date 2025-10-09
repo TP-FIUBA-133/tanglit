6dcb730d32fa9a8384e19153f42788dcb4c2f65a
Christian Pedersen <chrishaig14@gmail.com>
2025-10-09 17:01:25 -0300

    add buttons in main menu

--- frontend/src/MainMenu.vue
+++ frontend/src/MainMenu.vue
@@ -1,5 +1,14 @@
 <script setup lang="ts">
-defineEmits(["load_sample_markdown", "file_selected", "preview_slides", "preview_html", "save_file", "open_file"]);
+defineEmits([
+  "load_sample_markdown",
+  "file_selected",
+  "preview_slides",
+  "preview_html",
+  "save_file",
+  "open_file",
+  "save_html",
+  "save_pdf",
+]);
 </script>
 
 <template>
@@ -8,7 +17,9 @@ defineEmits(["load_sample_markdown", "file_selected", "preview_slides", "preview
     <button title="Save" @click="$emit('save_file')">Save</button>
     <button title="Load sample markdown" @click="$emit('load_sample_markdown')">Sample markdown</button>
     <button title="Preview slides" @click="$emit('preview_slides')">Preview slides</button>
-    <button title="Export to doc" @click="$emit('preview_html')">Export doc</button>
+    <button title="Export to doc" @click="$emit('preview_html')">Preview doc</button>
+    <button title="Save" @click="$emit('save_html')">Save doc as HTML</button>
+    <button title="Save" @click="$emit('save_pdf')">Save doc as PDF</button>
     <button title="Tangle code">Tangle code</button>
   </div>
 </template>
